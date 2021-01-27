92882934c2ff027e9fc162fbe882a0576d1092c2
sergioesco27 <moisesescobar355@gmail.com>
2021-01-26 23:06:01 -0500

    Segundo Cambio Realizado

--- src/main/java/com/istvin/colaborativo/deber.java
+++ src/main/java/com/istvin/colaborativo/deber.java
@@ -20,6 +20,8 @@ public class deber {
         System.out.println("Integrantes");
         System.out.println("Sergio Escobar");
         System.out.println("Paul Guevara");
+        System.out.println("Desarrollo de  Software");
+        System.out.println("Segundo Semestre");
     }
     
 }
